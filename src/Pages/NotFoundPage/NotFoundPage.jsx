b29f744e34117eb1af38cea4a45b443689b68c1b
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-08-10 20:49:20 +0300

    Create NotFoundPage

---
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+export default function NotFoundPage() {
+      useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+  return (
+    <div>
+      NotFound
+    </div>
+  )
+}
