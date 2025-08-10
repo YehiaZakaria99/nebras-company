061c64837298081b1c55787e6667956f1f4969ca
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-08-10 20:58:04 +0300

    Edit AppJS

---
+++ src/Pages/OurExperiencePages/OurExperiencePage.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+export default function OurExperiencePage() {
+      useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+  return (
+    <div>
+      OurExperience
+    </div>
+  )
+}
