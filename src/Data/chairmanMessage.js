358d6f283b6d0556c1ed4ce704a69f77f3e65587
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-08-16 20:28:11 +0300

    add chairmanMessage

---
+++ src/Data/chairmanMessage.js
@@ -0,0 +1,28 @@
+const chairmanMessage = {
+  title: "Chairman’s Message",
+  shortMsg: `It is with great pride that I extend my greetings to you on behalf of Earth Cresset Contracting Co.
+    Over the past two decades, our company has built a solid reputation as a trusted partner in delivering high-quality,
+    cost-efficient, and timely solutions in the fields of construction, pipelines, and mechanical works across the Kingdom....`,
+  longMsg: `
+    It is with great pride that I extend my greetings to you on behalf of Earth Cresset Contracting Co.
+    Over the past two decades, our company has built a solid reputation as a trusted partner in delivering high-quality,
+    cost-efficient, and timely solutions in the fields of construction, pipelines, and mechanical works across the Kingdom.
+    Our success has been driven by an unwavering commitment to excellence, innovation, and integrity.
+    We firmly believe that every project we undertake is not just about building infrastructure,
+    but also about building long-term partnerships based on trust, transparency, and mutual respect.
+
+    At Earth Cresset, we place our clients at the heart of everything we do.
+    Guided by a vision for sustainable growth and continuous improvement,
+    we strive to exceed expectations by combining our technical expertise with a deep understanding of our clients’ unique needs.
+    Looking ahead, we remain dedicated to expanding our capabilities, embracing new technologies,
+    and investing in our people to ensure that we continue to play a leading role in shaping the future of our industry.
+
+    On behalf of our entire team, I would like to thank our clients, partners,
+    and employees for their continued trust and support. Together,
+    we will continue to achieve greater milestones and contribute to the development and prosperity of the Kingdom.
+  `,
+  author: "Mr. Mutab Al Hajri",
+  position: "Chairman & CEO",
+};
+
+export default chairmanMessage;
