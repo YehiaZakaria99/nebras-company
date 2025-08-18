7efa4a7a1865934935219124d2e847354866b5a7
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-08-18 07:10:23 +0300

    Edit Styles for OurSoulutions Section

--- src/Sections/OurSolutionsSection.jsx
+++ src/Sections/OurSolutionsSection.jsx
@@ -4,72 +4,69 @@ import mechanicalImg from "/GalleryPage/31.webp";
 import electricalImg from "/GalleryPage/34.webp";
 import technicalImg from "/GalleryPage/32.webp";
 
-import {
-  ArrowRight,
-  Building2,
-  Cable,
-  Settings,
-  Wrench,
-} from "lucide-react";
+import { ArrowRight, Building2, Cable, Settings, Wrench } from "lucide-react";
 import { Link } from "react-router";
 import { cn } from "../lib/utils";
 import { useEffect, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import IntroOverlay from "../Components/IntroOverlay/IntroOverlay";
+import AnimatedOverlay from "../Components/AnimatedOverlay/AnimatedOverlay";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const solutions = [
+  {
+    icon: <Building2 size={36} />,
+    title: "Construction Solutions",
+    description:
+      "Comprehensive civil and construction services tailored to industrial needs.",
+    bgImg: constructionImg,
+  },
+  {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36px"
+        viewBox="0 0 48 48"
+        fill="currentColor"
+      >
+        <path d="M40,30a2,2,0,0,0-2,2v1H22a2,2,0,0,1,0-4h4A12,12,0,0,0,26,5H10V4A2,2,0,0,0,6,4V16a2,2,0,0,0,4,0V15H26a2,2,0,0,1,0,4H22a12,12,0,0,0,0,24H38v1a2,2,0,0,0,4,0V32A2,2,0,0,0,40,30Z" />
+      </svg>
+    ),
+    title: "Pipelines Solutions",
+    description:
+      "Expertise in pipeline installation, maintenance, and inspection.",
+    bgImg: pipeLineImg,
+  },
+  {
+    icon: <Settings size={36} />,
+    title: "Mechanical Solutions",
+    description:
+      "Innovative mechanical systems and equipment for industrial operations.",
+    bgImg: mechanicalImg,
+  },
+  // {
+  //   icon: <Cable size={36} />,
+  //   title: "Electrical Solutions",
+  //   description:
+  //     "High-quality electrical systems, installations, and automation.",
+  //   bgImg: electricalImg,
+  // },
+  // {
+  //   icon: <Wrench size={36} />,
+  //   title: "Technical Solutions",
+  //   description:
+  //     "Specialized technical support and custom-engineered solutions.",
+  //   bgImg: technicalImg,
+  // },
+];
+
 export default function OurSolutionsSection() {
   const titleRef = useRef(null);
-
-  const solutions = [
-    {
-      icon: <Building2 size={36} />,
-      title: "Construction Solutions",
-      description:
-        "Comprehensive civil and construction services tailored to industrial needs.",
-      bgImg: constructionImg,
-    },
-    {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="36px"
-          viewBox="0 0 48 48"
-          fill="currentColor"
-        >
-          <path d="M40,30a2,2,0,0,0-2,2v1H22a2,2,0,0,1,0-4h4A12,12,0,0,0,26,5H10V4A2,2,0,0,0,6,4V16a2,2,0,0,0,4,0V15H26a2,2,0,0,1,0,4H22a12,12,0,0,0,0,24H38v1a2,2,0,0,0,4,0V32A2,2,0,0,0,40,30Z" />
-        </svg>
-      ),
-      title: "Pipelines Solutions",
-      description:
-        "Expertise in pipeline installation, maintenance, and inspection.",
-      bgImg: pipeLineImg,
-    },
-    {
-      icon: <Settings size={36} />,
-      title: "Mechanical Solutions",
-      description:
-        "Innovative mechanical systems and equipment for industrial operations.",
-      bgImg: mechanicalImg,
-    },
-    {
-      icon: <Cable size={36} />,
-      title: "Electrical Solutions",
-      description:
-        "High-quality electrical systems, installations, and automation.",
-      bgImg: electricalImg,
-    },
-    {
-      icon: <Wrench size={36} />,
-      title: "Technical Solutions",
-      description:
-        "Specialized technical support and custom-engineered solutions.",
-      bgImg: technicalImg,
-    },
-  ];
-  const scrollRef = useRef(null);
+  const imgRef = useRef(null);
+  const overlayImageRef = useRef(null);
   const btnRef = useRef(null);
 
   useEffect(() => {
@@ -81,107 +78,43 @@ export default function OurSolutionsSection() {
 
   // Animation
 
-  useGSAP(() => {
-    const boxes = gsap.utils.toArray(scrollRef.current.children);
-
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: scrollRef.current,
-          start: "top 85%",
-          // end: "center 49%",
-          toggleActions: "play none none reverse",
-        },
-      })
-      .fromTo(
-        boxes,
-        {
-          // scale: 0.1,
-          rotationY: 180,
-          opacity: 0,
-          transformPerspective: 1000,
-          // x: "10px",
-          duration: 0.6,
-          ease: "power2.inOut",
-          stagger: 0.15,
-          willChange: "opacity, transform",
-        },
-        {
-          // scale: 1,
-          rotationY: 0,
-          opacity: 1,
-          // x: 0,
-          duration: 0.6,
-          stagger: 0.15,
-          ease: "power2.inOut",
-          clearProps: "willChange",
-        }
-      );
-
-    // btn animation
-    gsap.fromTo(
-      btnRef.current,
-      {
-        x: "-400px",
-        opacity: 0,
-      },
-      {
-        x: 0,
-        duration: 0.6,
-        ease: "power2.inOut",
-        opacity: 1,
-        scrollTrigger: {
-          trigger: btnRef.current,
-          start: "top 97%",
-          toggleActions: "play none none reverse",
-        },
-      }
-    );
-  }, []);
-
-  useGSAP(() => {
-    gsap.from([titleRef.current /*subtitleRef.current */], {
-      y: "100px",
-      opacity: 0,
-      duration: 1,
-      stagger: 0.2,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: titleRef.current,
-        start: "top 90%",
-      },
-    });
-  }, []);
-
   return (
     <section className="py-16 bg-gray-100 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 text-center">
         <div className="mb-12">
           <h3
             ref={titleRef}
-            className="text-3xl md:text-4xl font-extrabold text-primary tracking-wide mb-2"
+            className="text-3xl md:text-5xl font-extrabold text-hover tracking-wide mb-2"
           >
             Our Solutions
           </h3>
-          <div className="w-24 h-[3px] bg-primary mx-auto" />
+          <div className="w-24 h-[3px] bg-hover mx-auto" />
+          <p className="text-center max-w-[30rem] mx-auto font-semibold my-6 text-lg  md:text-xl text-primary/90">
+            We deliver solutions that combine accuracy with profound knowledge,
+            ensuring our clients’ toughest challenges are addressed with
+            efficiency and excellence.
+          </p>
         </div>
 
         <div
-          ref={scrollRef}
-          className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
+          // ref={scrollRef}
+          // 
+          className="grid sm:grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 md:px-0 lg:px-10 py-10"
         >
           {solutions.map((solution, idx) => (
             <div
               style={{ transformStyle: "preserve-3d", perspective: "1000px" }}
               key={idx}
               className={cn(
-                "bg-white rounded-2xl overflow-hidden shadow-md h-full border-primary text-primary",
-                "transition-all duration-300 transform group hover:bg-primary hover:text-white",
-                "relative"
+                "bg-white rounded-2xl overflow-hidden  shadow-md h-[30rem] border-hover ",
+                "transition-all duration-300 transform group ",
+                "relative hover:scale-[1.02] ",
+                idx % 2 === 0 ? "hover:rotate-2" : "hover:-rotate-2"
               )}
             >
-              <div className="img-box h-full relative">
+              <div className="img-box h-full relative  ">
                 <img
+                  // ref={imgRef}
                   loading="lazy"
                   className={cn(
                     "w-full h-full object-cover ",
@@ -190,9 +123,11 @@ export default function OurSolutionsSection() {
                   src={solution.bgImg}
                   alt={solution.title}
                 />
+                <AnimatedOverlay imgRef={imgRef} />
+
                 <div
                   className={cn(
-                    "overlay absolute inset-0 z-10 bg-primary/70",
+                    "overlay absolute inset-0 z-10 bg-hover/50",
                     "transition duration-300"
                   )}
                 ></div>
@@ -200,15 +135,15 @@ export default function OurSolutionsSection() {
               <div
                 className={cn(
                   "content flex flex-col items-center gap-3 text-white",
-                  "absolute z-20 bottom-5 left-1/2",
+                  "absolute z-20 bottom-32 left-1/2",
                   "transform -translate-x-1/2",
                   "will-change-transform"
                 )}
               >
-                <div className="transform group-hover:-translate-y-5 transition-all duration-300">
+                {/* <div className="transform group-hover:-translate-y-5 transition-all duration-300">
                   {solution.icon}
-                </div>
-                <h3 className="text-lg font-bold transform group-hover:-translate-y-5 transition-all duration-300">
+                </div> */}
+                <h3 className="text-xl md:text-2xl lg:text-4xl font-bold transform group-hover:-translate-y-5 transition-all duration-300">
                   {solution.title}
                 </h3>
                 {/* <p className="text-sm ">{solution.description}</p> */}
@@ -223,11 +158,11 @@ export default function OurSolutionsSection() {
             ref={btnRef}
             to="/ourSolutions"
             className={cn(
-              "inline-flex items-center transform  gap-2 text-md px-6 py-2 rounded-md border border-primary text-primary",
-              "hover:bg-primary hover:text-white transition-all duration-300 font-semibold group"
+              "inline-flex items-center transform  gap-2 text-md px-6 py-2 rounded-md border border-hover text-hover",
+              "hover:bg-hover hover:text-white transition-all duration-300 font-bold group"
             )}
           >
-            More Details
+            See More
             <ArrowRight className="w-4 h-4 transition-transform duration-300 group-hover:translate-x-1" />
           </Link>
         </div>
