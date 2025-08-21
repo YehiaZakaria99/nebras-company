4082e5b871da2136179356bb3cce6fade52bdc13
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-08-21 19:13:46 +0300

    Add Images to Equipments Page

--- src/Pages/AboutPages/OurEquipments.jsx
+++ src/Pages/AboutPages/OurEquipments.jsx
@@ -1,28 +1,45 @@
 // images
 import bgImg from "/AboutPages/our-equipments/bgImg.webp";
 
-import img1 from "/AboutPages/our-equipments/img.jpeg";
+import img1 from "/AboutPages/our-equipments/1.webp";
+import img2 from "/AboutPages/our-equipments/2.webp";
+import img3 from "/AboutPages/our-equipments/3.webp";
+import img4 from "/AboutPages/our-equipments/4.webp";
+import img5 from "/AboutPages/our-equipments/5.webp";
+import img6 from "/AboutPages/our-equipments/6.webp";
+import img7 from "/AboutPages/our-equipments/7.webp";
+import img8 from "/AboutPages/our-equipments/8.webp";
+import img9 from "/AboutPages/our-equipments/9.webp";
+import img10 from "/AboutPages/our-equipments/10.webp";
+import img11 from "/AboutPages/our-equipments/11.webp";
+import img12 from "/AboutPages/our-equipments/12.webp";
+import img13 from "/AboutPages/our-equipments/13.webp";
+import img14 from "/AboutPages/our-equipments/14.webp";
 
 import { useEffect, useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { cn } from "../../lib/utils";
+import IntroOverlay from "../../Components/IntroOverlay/IntroOverlay";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function OurEquipments() {
   const data = [
     { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
-    { title: "Lorem ipsum dolor sit.", img: img1 },
+    { title: "Lorem ipsum dolor sit.", img: img2 },
+    { title: "Lorem ipsum dolor sit.", img: img3 },
+    { title: "Lorem ipsum dolor sit.", img: img4 },
+    { title: "Lorem ipsum dolor sit.", img: img5 },
+    { title: "Lorem ipsum dolor sit.", img: img6 },
+    { title: "Lorem ipsum dolor sit.", img: img7 },
+    { title: "Lorem ipsum dolor sit.", img: img8 },
+    { title: "Lorem ipsum dolor sit.", img: img9 },
+    { title: "Lorem ipsum dolor sit.", img: img10 },
+    { title: "Lorem ipsum dolor sit.", img: img11 },
+    // { title: "Lorem ipsum dolor sit.", img: img12 },
+    // { title: "Lorem ipsum dolor sit.", img: img13 },
+    { title: "Lorem ipsum dolor sit.", img: img14 },
   ];
   useEffect(() => {
     data.forEach((item) => {
@@ -46,11 +63,11 @@ export default function OurEquipments() {
       gsap.to(card, {
         y: -20 * (cards.length - index),
         ease: "power2.inOut",
-        duration: .6,
+        duration: 0.6,
         scrollTrigger: {
           trigger: card,
           start: "top 90%",
-          end: "bottom top",
+          end: "bottom 10%",
           scrub: true,
         },
       });
@@ -63,11 +80,11 @@ export default function OurEquipments() {
           scale: 1,
           rotationY: 0,
           y: 0,
-          duration: .6,
+          duration: 0.6,
           ease: "power2.inOut",
           scrollTrigger: {
             trigger: image,
-            start: "top 90%",
+            start: "top 80%",
             end: "top 40%",
             scrub: true,
           },
@@ -82,14 +99,20 @@ export default function OurEquipments() {
 
   return (
     <>
-      <section className="our-equipments">
+      <section className="relative our-equipments">
+        <IntroOverlay />
         <section
-          className="mb-20 relative px-6 md:px-16 py-10  flex flex-col-reverse md:flex-row items-center gap-10 bg-cover h-[300px] md:h-[450px] lg:h-[550px] md:bg-fixed bg-center  shadow-xl overflow-hidden"
+          className="mb-20 relative px-6 md:px-16 py-5 md:py-10  flex flex-col-reverse md:flex-row items-center gap-10 bg-cover h-[300px] md:h-[450px] lg:h-[550px] md:bg-fixed bg-center  shadow-xl overflow-hidden"
           style={{ backgroundImage: `url(${bgImg})` }}
         >
-          <div className="absolute inset-0 bg-gradient-to-r from-primary/90 via-hover/30 to-primary/80 z-10" />
+          <div className="absolute inset-0 bg-gradient-to-b from-white/80 to-hover/70 z-10" />
           <div className="md:w-1/2 space-y-4 z-20 text-center md:text-left">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-bg tracking-tight leading-tight drop-shadow-lg">
+            <h1
+              className={cn(
+                "text-4xl md:text-5xl lg:text-6xl font-extrabold text-bg tracking-tight leading-tight drop-shadow-lg",
+                "text-secColor"
+              )}
+            >
               Our Equipments
             </h1>
             <p className="text-white text-lg md:text-2xl max-w-md font-bold">
@@ -98,10 +121,14 @@ export default function OurEquipments() {
             </p>
           </div>
         </section>
-        <section className="  py-20">
+        <section className=" py-20">
           <div
             ref={wrapperRef}
-            className="cards-wrapper max-w-6xl mx-auto px-4 sm:px-6 grid sm:grid-cols-2 gap-12 relative z-10 "
+            className={cn(
+              "cards-wrapper max-w-6xl mx-auto px-4 sm:px-6 grid sm:grid-cols-2 gap-12 relative z-10",
+              "shadow-xl shadow-hover overflow-hidden rounded-lg",
+              "py-10"
+            )}
           >
             {data.map((item, index) => (
               <div
@@ -110,7 +137,7 @@ export default function OurEquipments() {
                   "card-item  rounded-3xl p-6 sm:p-8  relative z-[1] transition-transform duration-300 will-change-transform "
                 )}
               >
-                <div className="card-image-container max-h-80 overflow-hidden rounded-2xl mb-4">
+                <div className="card-image-container max-h-80 h-80  overflow-hidden rounded-2xl mb-4 ">
                   <img
                     style={{
                       transformStyle: "preserve-3d",
@@ -118,10 +145,13 @@ export default function OurEquipments() {
                     }}
                     src={item.img}
                     alt={item.title}
-                    className="card-image w-full h-full object-cover transition-all duration-300 rounded-2xl"
+                    className={cn(
+                      "card-image w-full h-full object-contain object-center transition-all duration-300 rounded-2xl",
+                      ""
+                    )}
                   />
                 </div>
-                <h3 className="text-xl sm:text-2xl font-semibold text-center text-primary mt-5">
+                <h3 className="text-xl sm:text-2xl font-semibold text-center text-secColor mt-5 truncate">
                   {item.title}
                 </h3>
               </div>
