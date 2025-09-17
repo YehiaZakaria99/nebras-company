d180948507b79479bce9c8dd3cbc55030d2a839b
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-17 21:01:03 +0300

    add oursolutions page

--- src/Pages/OurSolutionsPage/OurSolutionsPage.jsx
+++ src/Pages/OurSolutionsPage/OurSolutionsPage.jsx
@@ -19,8 +19,6 @@ export default function OurSolutionsPage() {
   //   window.scrollTo(0, 0);
   // }, [pathname]);
 
-
-
   const solutions = [
     {
       id: 0,
@@ -44,72 +42,73 @@ export default function OurSolutionsPage() {
       description: `We provide a full range of mechanical services; including equipment installation,
       maintenance, and repair, with a specific focus on air conditioning and refrigeration systems.`,
     },
-    {
-      id: 3,
-      img: electricalImg,
-      title: "Electrical Solutions",
-      description: `
-Generators and Automatic changeover Panels,
-Cable Laying and Termination,
-Switchgears, Control centers and Specialized Panels,
-Power Distribution Systems,
-Lighting and lighting control Systems,
-Lighting Protection and Earthing,
-Uninterruptible Power Supply Systems,
-PLC panels & instrumentation
+    //     {
+    //       id: 3,
+    //       img: electricalImg,
+    //       title: "Electrical Solutions",
+    //       description: `
+    // Generators and Automatic changeover Panels,
+    // Cable Laying and Termination,
+    // Switchgears, Control centers and Specialized Panels,
+    // Power Distribution Systems,
+    // Lighting and lighting control Systems,
+    // Lighting Protection and Earthing,
+    // Uninterruptible Power Supply Systems,
+    // PLC panels & instrumentation
 
-`,
-    },
-    {
-      id: 4,
-      img: technicalImg,
-      title: "Technical Solutions",
-      description: `
-      Automation Systems,
-      Fire Alarm Systems,
-      Data Networking & Structured Cabling (Telephone & Data),
-      Fiber Optic Cabling & Transmission,
-      Wireless solutions,
-      Telecommunications,
-      Audio Visual Systems,
-      CCTV,MATV & SMATV Systems,
-      Access Control System & Gate Barrier Systems,
-      Public Address System & Intercom Systems,
-      Emergency Lighting Systems,
-      Central Battery System / UPS,
-      Intrusion Alarm System,
-      Nurse Call System.
+    // `,
+    //     },
+    //     {
+    //       id: 4,
+    //       img: technicalImg,
+    //       title: "Technical Solutions",
+    //       description: `
+    //       Automation Systems,
+    //       Fire Alarm Systems,
+    //       Data Networking & Structured Cabling (Telephone & Data),
+    //       Fiber Optic Cabling & Transmission,
+    //       Wireless solutions,
+    //       Telecommunications,
+    //       Audio Visual Systems,
+    //       CCTV,MATV & SMATV Systems,
+    //       Access Control System & Gate Barrier Systems,
+    //       Public Address System & Intercom Systems,
+    //       Emergency Lighting Systems,
+    //       Central Battery System / UPS,
+    //       Intrusion Alarm System,
+    //       Nurse Call System.
 
-`,
-    },
+    // `,
+    //     },
   ];
   return (
     <>
-      <section className="oursolutions-page py-20">
+      <section className="oursolutions-page pb-20">
         {/* Header Section */}
-        <section className="bg-primary/20">
-          <div
-            className={cn(
-              "relative px-6 md:px-16 py-10 mb-20 shadow-2xl overflow-hidden",
-              "flex flex-col-reverse md:flex-row items-center gap-10",
-              "h-[300px] md:h-[450px] lg:h-[500px]",
-              "bg-cover bg-bottom md:bg-fixed"
-            )}
-            style={{ backgroundImage: `url(${bgImg})` }}
-          >
-            <div className="absolute inset-0 bg-gradient-to-r from-primary/90 via-hover/30 to-primary/80 z-10 animate-fade-in" />
-            <div className="md:w-1/2 space-y-4 z-20 text-center md:text-left">
-              <h1 className="text-bg text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight leading-tight drop-shadow-xl">
-                Our Solutions
-              </h1>
-              <p
-                className="text-white max-w-3xl mx-auto text-xl md:text-2xl font-bold leading-relaxed"
-                data-aos="fade-left"
-              >
-                We offer a wide range of services, with our core business
-                focused on earthwork activities
-              </p>
-            </div>
+        <section
+          className={cn(
+            "mb-20 relative px-6 md:px-16 py-5 md:py-10 bg-cover h-[300px] md:h-[450px] lg:h-[550px] md:bg-fixed bg-center  shadow-xl overflow-hidden",
+            "flex flex-col-reverse md:flex-row items-center gap-10"
+          )}
+          style={{ backgroundImage: `url(${bgImg})` }}
+        >
+          <div className="absolute inset-0 bg-gradient-to-br from-black/80 to-hover/80 z-10" />
+          <div className="md:w-1/2 space-y-4 z-20 text-center md:text-left">
+            <h1
+              className={cn(
+                "text-4xl md:text-5xl lg:text-6xl font-extrabold text-bg tracking-tight leading-tight drop-shadow-lg",
+                "text-secColor"
+              )}
+            >
+              Our Solutions
+            </h1>
+            <p
+              className="text-white text-lg md:text-2xl max-w-md font-bold"
+              data-aos="fade-left"
+            >
+              We offer a wide range of services, with our core business focused
+              on earthwork activities
+            </p>
           </div>
         </section>
         <section className="space-y-28 overflow-hidden py-5">
