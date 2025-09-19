28a7d18161ade22e348657ac631321a088e8d16e
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-19 20:50:50 +0300

    Add Map to Contact section

--- src/Sections/ContactSection.jsx
+++ src/Sections/ContactSection.jsx
@@ -2,6 +2,7 @@ import { Mail, MapPin, Phone, ArrowRight } from "lucide-react";
 import { Link } from "react-router";
 import { cn } from "./../lib/utils";
 import ContactCard from "../Components/ContactCard/ContactCard";
+import Map from "../Components/Map/Map";
 
 const contactData = [
   {
@@ -31,7 +32,7 @@ export default function ContactSection() {
       className="relative bg-white py-24 px-4 md:px-12 lg:px-24 xl:px-32 overflow-hidden"
     >
       <div className="relative z-10 max-w-6xl mx-auto text-center">
-        {/* header */}
+        {/* Header */}
         <div className="mb-12">
           <h3 className="text-3xl md:text-5xl font-extrabold text-secColor tracking-wide mb-2">
             Get In Touch
@@ -41,22 +42,11 @@ export default function ContactSection() {
             Have a question or need assistance? We're just one message away.
           </p>
         </div>
-        {/* map */}
-        <div
-          id="location"
-          className="rounded-3xl overflow-hidden shadow-md shadow-secColor/30 border border-secColor/10 mb-16"
-        >
-          <iframe
-            title="GSP Location"
-            className="w-full h-[400px]"
-            loading="lazy"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            referrerPolicy="no-referrer-when-downgrade"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d461.091659127313!2d50.214730012894556!3d26.30266000118424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e957c1bb640b%3A0xfc1af782797e7f8b!2sBANDAREYAH%20TRADING%20CENTER!5e0!3m2!1sar!2seg!4v1758296621271!5m2!1sar!2seg"
-          ></iframe>
-        </div>
-        {/* contact cards */}
+
+        {/* Map */}
+        <Map />
+
+        {/* Contact Cards */}
         <div className="grid gap-8 md:grid-cols-3 mb-16">
           {contactData.map(({ icon, title, details }, idx) => (
             <div
