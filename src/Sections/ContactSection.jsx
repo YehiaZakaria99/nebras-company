bdd965a28219ee8ecd08d1dc1b84f02a6a12e456
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-19 04:48:47 +0300

    Add Contact Card To Section

--- src/Sections/ContactSection.jsx
+++ src/Sections/ContactSection.jsx
@@ -1,27 +1,52 @@
 import { Mail, MapPin, Phone, ArrowRight } from "lucide-react";
 import { Link } from "react-router";
 import { cn } from "./../lib/utils";
+import ContactCard from "../Components/ContactCard/ContactCard";
+
+const contactData = [
+  {
+    icon: <Mail size={24} />,
+    title: "Emails",
+    details: ["aaaaa@gmail.com"],
+  },
+  {
+    icon: <Phone size={24} />,
+    title: "Phone",
+    details: ["01234567890"],
+  },
+  {
+    icon: <MapPin size={24} />,
+    title: "Location",
+    details: [
+      "Al Bandariyah Business Centre, Office No 105,",
+      "Al Amir Faisal Bin Fahd Rd, Khobar, K.S.A",
+    ],
+  },
+];
 
 export default function ContactSection() {
   return (
-    <section className="relative bg-navBg py-24 px-4 md:px-12 lg:px-24 xl:px-32 overflow-hidden">
-      <div className="absolute -top-20 -left-20 w-80 h-80 bg-mainColor/10 rounded-full blur-3xl z-0"></div>
-      <div className="absolute bottom-0 right-0 w-64 h-64 bg-mainColor/20 rounded-full blur-3xl z-0"></div>
+    <section id="contactUs" className="relative bg-white py-24 px-4 md:px-12 lg:px-24 xl:px-32 overflow-hidden">
+      {/* <div className="absolute top-0 left-0 w-full h-40 bg-secColor/80 rounded-full blur-3xl z-0"></div> */}
+      {/* <div className="absolute bottom-0 right-0 w-full h-80 bg-secColor/20 rounded-full blur-3xl z-0"></div> */}
 
       <div className="relative z-10 max-w-6xl mx-auto text-center">
         {/* header */}
         <div className="mb-12">
-          <h3 className="text-3xl md:text-4xl font-extrabold text-primary tracking-wide mb-2">
+          <h3 className="text-3xl md:text-5xl font-extrabold text-secColor tracking-wide mb-2">
             Get In Touch
           </h3>
-          <div className="w-24 h-[3px] bg-primary mx-auto" />
-          <p className="text-lg md:text-xl text-primary/80 mb-12 max-w-2xl mx-auto">
+          <div className="w-24 h-[3px] bg-secColor mx-auto" />
+          <p className="text-center max-w-[30rem] mx-auto font-semibold my-6 text-lg  md:text-xl text-primary/90">
             Have a question or need assistance? We're just one message away.
           </p>
         </div>
 
         {/* map */}
-        <div className="rounded-3xl overflow-hidden shadow-2xl border border-mainColor/10 mb-16">
+        <div
+          id="location"
+          className="rounded-3xl overflow-hidden shadow-sm shadow-secColor border border-secColor/10 mb-16"
+        >
           <iframe
             title="GSP Location"
             className="w-full h-[400px]"
@@ -31,69 +56,9 @@ export default function ContactSection() {
         </div>
 
         <div className="grid gap-8 md:grid-cols-3 mb-16">
-          {/* Email */}
-          <div className={cn("contact-sec-card", "group")}>
-            <div
-              className={cn(
-                "icon-box",
-                "group-hover:bg-white group-hover:text-primary"
-              )}
-            >
-              <Mail size={24} />
-            </div>
-            <p className="font-semibold text-base">Emails</p>
-            <div className="text-sm mt-1 space-y-1 text-center">
-              <a
-                href="mailto:info@gsp-sa.com"
-                className="hover:underline block"
-              >
-                info@gsp-sa.com
-              </a>
-              <a
-                href="mailto:mohammed@gsp-sa.com"
-                className="hover:underline block"
-              >
-                mohammed@gsp-sa.com
-              </a>
-            </div>
-          </div>
-
-          {/* Phone */}
-          <div className={cn("contact-sec-card", "group")}>
-            <div
-              className={cn(
-                "icon-box",
-                "group-hover:bg-white group-hover:text-primary"
-              )}
-            >
-              <Phone size={24} />
-            </div>
-            <p className="font-semibold text-base">Phone</p>
-            <a
-              href="tel:+966138813042"
-              className="text-sm hover:underline mt-1"
-            >
-              +966 13 881 3042
-            </a>
-          </div>
-
-          {/* Address */}
-          <div className={cn("contact-sec-card", "group")}>
-            <div
-              className={cn(
-                "icon-box",
-                "group-hover:bg-white group-hover:text-primary"
-              )}
-            >
-              <MapPin size={24} />
-            </div>
-            <p className="font-semibold text-base">Location</p>
-            <p className="text-sm mt-1 text-center">
-              Al Bandariyah Business Centre, Office No 105,
-              <br />
-              Al Amir Faisal Bin Fahd Rd, Khobar, K.S.A
-            </p>
-          </div>
+          {contactData.map(({ icon, title, details }, idx) => (
+            <ContactCard icon={icon} title={title} details={details} />
+          ))}
         </div>
 
         {/* CTA */}
@@ -101,8 +66,8 @@ export default function ContactSection() {
           <Link
             to="/contact"
             className={cn(
-              "group inline-flex gap-3 items-center text-md px-6 py-2 rounded-md border border-primary text-primary",
-              "hover:bg-primary hover:text-white transition-all duration-300 font-semibold"
+              "group inline-flex items-center transform  gap-2  md:px-8 md:py-4 text-xs md:text-sm lg:text-lg px-4 py-2 rounded-md border border-secColor text-secColor",
+              "hover:bg-secColor hover:text-white transition-all duration-300 font-semibold"
             )}
           >
             Send a Message
