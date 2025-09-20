a085d2961c49826f98f1ced7ad12da97531d5262
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-20 03:33:35 +0300

    Edit Map Zoom and memoization

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React, { memo, useLayoutEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-const Map = React.memo(() => {
+const Map = () => {
   useLayoutEffect(() => {
     const existingMap = L.DomUtil.get("nebras-map");
     if (existingMap && existingMap._leaflet_id) {
@@ -11,12 +11,12 @@ const Map = React.memo(() => {
 
     const map = L.map("nebras-map", {
       center: [26.30266, 50.21473],
-      zoom: 15,
+      zoom: 17,
     });
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      minZoom: 3,
       maxZoom: 19,
-      minZoom: 19,
       detectRetina: true,
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
@@ -24,23 +24,22 @@ const Map = React.memo(() => {
 
     L.marker([26.30266, 50.21473]).addTo(map).bindPopup("Nebras Location");
 
+    // إصلاح البكسلة / الرمادية
     setTimeout(() => {
       map.invalidateSize();
-    }, 200);
+    }, 100);
 
     return () => map.remove();
   }, []);
 
   return (
-    <>
-      <div
-        id="location"
-        className="rounded-3xl relative w-full overflow-hidden shadow-md shadow-secColor/30 border border-secColor/10 mb-16"
-      >
-        <div id="nebras-map" className="w-full h-[400px] rounded-3xl"></div>
-      </div>
-    </>
+    <div
+      id="location"
+      className="rounded-3xl relative w-full overflow-hidden shadow-md shadow-secColor/30 border border-secColor/10 mb-16"
+    >
+      <div id="nebras-map" className="w-full h-[400px] rounded-3xl"></div>
+    </div>
   );
-});
+};
 
 export default memo(Map);
