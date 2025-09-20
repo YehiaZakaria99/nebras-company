d515f0b140b7c826876598e32ef1d70f35718feb
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-20 03:45:24 +0300

    Fix marker icon issue

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -2,6 +2,18 @@ import React, { memo, useLayoutEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const Map = () => {
   useLayoutEffect(() => {
     const existingMap = L.DomUtil.get("nebras-map");
