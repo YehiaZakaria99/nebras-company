dc9d9f3168396fc6ea0dda52ea3a470b212f7be3
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-22 08:23:26 +0300

    Create Leadership Page

--- src/Pages/AboutPages/LeadershipPage.jsx
+++ src/Pages/AboutPages/LeadershipPage.jsx
@@ -1,6 +1,7 @@
 import { cn } from "../../lib/utils";
 import bgImg from "/AboutPages/LeadershipPage/img.webp";
-import chairmanImg from "/AboutPages/LeadershipPage/owner.webp";
+import chairmanMessage from "../../Data/chairmanMessage";
+import chairmanImg from "/HomePage/LeadershipSection/chairman.webp";
 import { useEffect } from "react";
 
 export default function LeadershipPage() {
@@ -9,36 +10,37 @@ export default function LeadershipPage() {
   // }, []);
   return (
     <>
-      <section className="leadership-page py-20 bg-primary/20 ">
+      <section className="leadership-page pb-0 ">
         {/* Header Section */}
-        <section className=" ">
-          <div
-            className={cn(
-              "relative px-6 md:px-16 mb-20 shadow-2xl overflow-hidden py-10",
-              "flex flex-col-reverse md:flex-row items-center gap-10",
-              "h-[300px] md:h-[450px] lg:h-[500px]",
-              "bg-cover bg-center md:bg-fixed"
-            )}
-            style={{ backgroundImage: `url(${bgImg})` }}
-          >
-            <div className="absolute inset-0 bg-gradient-to-r from-primary/90 via-hover/30 to-primary/80 z-10 animate-fade-in" />
-            <div className="md:w-1/2 space-y-4 z-20 text-center md:text-left">
-              <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-bg tracking-tight leading-tight drop-shadow-xl">
-                Leadership
-              </h1>
-            </div>
+        <section
+          className={cn(
+            "relative px-6 md:px-16 py-10 mb-20 flex flex-col-reverse md:flex-row items-center gap-10 bg-cover h-[300px] md:h-[450px] lg:h-[500px] md:bg-fixed bg-center shadow-xl overflow-hidden",
+            "header"
+          )}
+          style={{ backgroundImage: `url(${bgImg})` }}
+        >
+          <div className="absolute inset-0 bg-gradient-to-br from-black/90 to-secColor/40 z-10" />
+          <div className="md:w-1/2 space-y-4 z-20 text-center md:text-left">
+            <h1 className="text-secColor text-4xl md:text-5xl lg:text-6xl font-extrabold text-bg tracking-tight leading-tight drop-shadow-xl">
+              Our Leadership
+            </h1>
+            <p className="text-white text-lg md:text-2xl max-w-2xl font-bold">
+              Our leadership team brings together decades of expertise and
+              strategic insight, guiding the company with integrity and
+              dedication to sustainable growth.
+            </p>
           </div>
         </section>
 
         {/* Intro Section */}
         <section className="px-6 md:px-16 py-10 max-w-6xl mx-auto text-center md:text-left">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4 text-primary">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4 text-secColor">
             About Chairman
           </h2>
           <p className="text-primary font-medium text-lg leading-relaxed">
             <span className="block my-2">
-              <span className="font-bold">Mr. Mohammed Al Assmi</span> – The CEO
-              of this company. GSP is moving forward under his inspiring
+              <span className="font-bold">{chairmanMessage.author}</span> – The
+              CEO of this company. GSP is moving forward under his inspiring
               leadership and vision.
             </span>
             <span className="block my-2">
@@ -50,7 +52,7 @@ export default function LeadershipPage() {
 
         {/* Message Section */}
         <section className="px-6 md:px-16 py-16 ">
-          <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center shadow-lg rounded-2xl p-6 md:p-10 bg-white/80 backdrop-blur-sm">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 gap-12 items-center rounded-2xl p-6 md:p-10 shadow-lg shadow-secColor">
             {/* Image */}
             <div className="flex justify-center">
               <div className="relative rounded-2xl overflow-hidden group">
@@ -59,13 +61,13 @@ export default function LeadershipPage() {
                   src={chairmanImg}
                   alt="Company Owner"
                   className={cn(
-                    "shadow-xl w-[300px] h-[400px] object-cover transform ",
+                    "shadow-xl w-[400px] h-[500px] object-cover object-top transform ",
                     "transition duration-500 ease-in-out group-hover:scale-110 "
                   )}
                 />
                 <div
                   className={cn(
-                    "overlay absolute inset-0 bg-hover/30 rounded-2xl z-0",
+                    "overlay absolute inset-0 bg-secColor/30 rounded-2xl z-0",
                     "group-hover:bg-transparent transition duration-500 ease-in-out"
                   )}
                 ></div>
@@ -74,31 +76,22 @@ export default function LeadershipPage() {
 
             {/* Message */}
             <div className="overflow-auto px-2 py-2 animate-fade-up">
-              <h3 className="text-2xl md:text-3xl font-bold text-primary mb-4">
+              <h3 className="text-2xl md:text-3xl font-bold text-secColor mb-4">
                 Chairman's Message
               </h3>
               <div className="text-primary/80 font-medium text-lg space-y-4 leading-relaxed">
-                <p className="italic border-l-4 border-primary pl-4 bg-primary/5 rounded-md">
-                  "The key to the Group’s sustainable success is encouraging
-                  leadership at all levels, embracing diversity as a source of
-                  strength, remaining unified as we work toward achieving our
-                  vision, and supporting entrepreneurship as we innovate and add
-                  value to customers, community, and nation."
+                <p className="italic border-l-4 border-secColor p-4 bg-secColor/5 rounded-md">
+                  {`${chairmanMessage.longMsg}`}
                 </p>
-                <p>
-                  GSP has evolved into a multi-speciality company with a global
-                  presence. Our journey of over 22 years has been rooted in core
-                  values and guided by a strong ethical compass.
-                </p>
-                <p>
-                  These values—leadership, diversity, unity, integrity, and
-                  innovation—continue to shape our growth and define our future.
-                </p>
+                <p>{`${chairmanMessage.completedMsg}`}</p>
+                <p>{`${chairmanMessage.completedMsg2}`}</p>
                 <div className="mt-6">
                   <p className="font-bold text-primary text-md italic">
-                    Mohammed Shamrookh Al Assmi
+                    {chairmanMessage.author}
                   </p>
-                  <p className="text-primary/70 text-sm">Group Chairman</p>
+                  <p className="text-secColor text-sm">
+                    {chairmanMessage.title}
+                  </p>
                 </div>
               </div>
             </div>
