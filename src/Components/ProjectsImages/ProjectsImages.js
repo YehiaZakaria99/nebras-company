9fb45eb5318bac7186f0e387b6ba4f4695a722dc
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-27 21:18:43 +0300

    Add Projects Data

---
+++ src/Components/ProjectsImages/ProjectsImages.js
@@ -0,0 +1,46 @@
+import img01 from "/ProjectsPage/01.webp";
+import img02 from "/ProjectsPage/02.webp";
+import img03 from "/ProjectsPage/03.webp";
+import img04 from "/ProjectsPage/04.webp";
+import img05 from "/ProjectsPage/05.webp";
+import img06 from "/ProjectsPage/06.webp";
+import img07 from "/ProjectsPage/07.webp";
+import img08 from "/ProjectsPage/08.webp";
+import img09 from "/ProjectsPage/09.webp";
+import img10 from "/ProjectsPage/10.webp";
+import img11 from "/ProjectsPage/11.webp";
+import img12 from "/ProjectsPage/12.webp";
+import img13 from "/ProjectsPage/13.webp";
+import img14 from "/ProjectsPage/14.webp";
+import img15 from "/ProjectsPage/15.webp";
+import img16 from "/ProjectsPage/16.webp";
+import img17 from "/ProjectsPage/17.webp";
+import img18 from "/ProjectsPage/18.webp";
+import img19 from "/ProjectsPage/19.webp";
+import img20 from "/ProjectsPage/20.webp";
+
+const projectsImages = [
+  { src: img01, alt: "project01" },
+  { src: img02, alt: "project02" },
+  { src: img03, alt: "project03" },
+  { src: img04, alt: "project04" },
+  { src: img05, alt: "project05" },
+  { src: img06, alt: "project06" },
+  { src: img07, alt: "project07" },
+  { src: img08, alt: "project08" },
+  { src: img09, alt: "project09" },
+  { src: img10, alt: "project10" },
+  { src: img11, alt: "project11" },
+  { src: img12, alt: "project12" },
+  { src: img13, alt: "project13" },
+  { src: img14, alt: "project14" },
+  { src: img15, alt: "project15" },
+  { src: img16, alt: "project16" },
+  { src: img17, alt: "project17" },
+  { src: img18, alt: "project18" },
+  { src: img19, alt: "project19" },
+  { src: img20, alt: "project20" },
+];
+
+
+export default projectsImages
