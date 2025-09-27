61cfa2a30744868223c474429674c78281f7482b
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-27 14:35:32 +0300

    Add Organizational Chart to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
-import Navbar from "./Components/Navbar/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./Pages/LayoutPage/Layout";
 import AboutPage from "./Pages/AboutPages/AboutPage";
-import OurExperiencePage from "./Pages/OurExperiencePages/OurExperiencePage";
+// import OurExperiencePage from "./Pages/OurExperiencePages/OurExperiencePage";
 import GalleryPage from "./Pages/GalleryPage/GalleryPage";
 import ContactPage from "./Pages/ContactPage/ContactPage";
 import NotFoundPage from "./Pages/NotFoundPage/NotFoundPage";
-import GSPInformation from "./Pages/AboutPages/GSPInformation";
+// import GSPInformation from "./Pages/AboutPages/GSPInformation";
 import OrganizationalChart from "./Pages/AboutPages/OrganizationalChart";
-import OurManPower from "./Pages/AboutPages/OurManPower";
-import OwnedMachines from "./Pages/AboutPages/OwnedMachines";
-import FinancialStatus from "./Pages/AboutPages/FinancialStatus";
+// import OurManPower from "./Pages/AboutPages/OurManPower";
+// import OwnedMachines from "./Pages/AboutPages/OwnedMachines";
+// import FinancialStatus from "./Pages/AboutPages/FinancialStatus";
 import LeadershipPage from "./Pages/AboutPages/LeadershipPage";
-import GalleryContextProvider from "./Context/GalleryContext";
+// import GalleryContextProvider from "./Context/GalleryContext";
 import OurEquipments from "./Pages/AboutPages/OurEquipments";
-import GSPIntroduction from "./Pages/AboutPages/GSPIntroduction/GSPIntroduction";
+// import GSPIntroduction from "./Pages/AboutPages/GSPIntroduction/GSPIntroduction";
 import OurServicesPage from "./Pages/OurServicesPage/OurServicesPage";
+
 
 function App() {
   const routers = createBrowserRouter([
@@ -31,14 +31,14 @@ function App() {
           path: "about",
           element: <AboutPage />,
           children: [
-            { path: "/about/our-equipments", element: <OurEquipments /> },
-            { path: "/about/leadership", element: <LeadershipPage /> },
+            { path: "our-equipments", element: <OurEquipments /> },
+            { path: "leadership", element: <LeadershipPage /> },
+            {
+              path: "organizational-chart",
+              element: <OrganizationalChart />,
+            },
             // { path: "/about/introduction", element: <GSPIntroduction /> },
             // { path: "/about/information", element: <GSPInformation /> },
-            // {
-            //   path: "/about/organizational-chart",
-            //   element: <OrganizationalChart />,
-            // },
             // { path: "/about/our-man-power", element: <OurManPower /> },
             // { path: "/about/owned-machines", element: <OwnedMachines /> },
             // { path: "/about/financial-status", element: <FinancialStatus /> },
@@ -58,7 +58,7 @@ function App() {
         { path: "*", element: <NotFoundPage /> },
       ],
     },
-  ]); 
+  ]);
   return (
     <>
       <RouterProvider router={routers} />
