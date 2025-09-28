618b240fd97715d57b50dde0581d678eee88da2d
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-28 05:32:52 +0300

    collect futureData in seperated file

---
+++ src/Data/futureData.js
@@ -0,0 +1,19 @@
+import ourVisionImg from "/HomePage/FutureSection/our-vision.webp";
+import ourMissionImg from "/HomePage/FutureSection/our-mission.webp";
+
+const futureSectionCardData = {
+  vision: {
+    id: 1,
+    title: "Our-Vision",
+    imgSrc: ourVisionImg,
+    desc: `To lead with innovation and set new standards of excellence.`,
+  },
+  mission: {
+    id: 2,
+    title: "Our-Mission",
+    imgSrc: ourMissionImg,
+    desc: `To deliver reliable solutions that create lasting value.`,
+  },
+};
+
+export default futureSectionCardData;
