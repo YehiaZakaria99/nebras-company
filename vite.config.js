bdce1740fc08b3cbbc08d6e1db2ef1891c7b0280
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-28 06:56:34 +0300

    use Manual Chunks in vite and add visualizer from rollup-plugin-visualizer to prevent chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // بيعمل تقرير بعد build تلاقيه في dist/stats.html
+    visualizer({
+      filename: "dist/stats.html",
+      template: "treemap", // ممكن "sunburst" أو "network"
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          gsap: ["gsap", "@gsap/react"],
+          leaflet: ["leaflet"],
+          slick: ["react-slick", "slick-carousel"],
+          photo: ["react-photo-view"],
+          org: ["react-organizational-chart"],
+          pageflip: ["react-pageflip"],
+        },
+      },
+    },
+  },
+});
