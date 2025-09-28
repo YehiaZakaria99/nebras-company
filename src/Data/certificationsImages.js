9ffba5e00ee294ea54e46088b1793eabfe0e50f6
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-28 05:31:12 +0300

    create file of certifactions data

---
+++ src/Data/certificationsImages.js
@@ -0,0 +1,7 @@
+import ISO_45001 from "/HomePage/OurExperienceSection/ISO45001.webp";
+import ISO_9001 from "/HomePage/OurExperienceSection/ISO9001.webp";
+import ISO_14001 from "/HomePage/OurExperienceSection/ISO14001.webp";
+
+const certificationsImages = [ISO_45001, ISO_9001, ISO_14001];
+
+export default certificationsImages;
