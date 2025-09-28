69f15a403876745552b437430fc52ff81d202b41
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-09-28 05:32:28 +0300

    Add experience section in home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,17 +2,17 @@
 import { useEffect, useState } from "react";
 import ScrollUp from "../../Components/Scroll/ScrollUp";
 import { cn } from "../../lib/utils";
-import AboutSection from "../../Sections/AboutSection";
+// import AboutSection from "../../Sections/AboutSection";
 import LeadershipSection from "../../Sections/LeadershipSection";
 import HeroSection from "./../../Sections/HeroSection";
-import WhyChooseUs from "./../../Sections/WhyChooseUs";
 import ContactSection from "../../Sections/ContactSection";
-import GallerySection from "./../../Sections/GallerySection";
-import GalleryContextProvider from "../../Context/GalleryContext";
-import IntroOverlay from "../../Components/IntroOverlay/IntroOverlay";
-import Loading from "./../../Components/Loading/Loading";
+// import GallerySection from "./../../Sections/GallerySection";
+// import GalleryContextProvider from "../../Context/GalleryContext";
+// import IntroOverlay from "../../Components/IntroOverlay/IntroOverlay";
+// import Loading from "./../../Components/Loading/Loading";
 import OurServicesSection from "../../Sections/OurServicessSection";
 import FutureSection from "../../Sections/FutureSection";
+import OurExperienceSection from "../../Sections/OurExperienceSection";
 
 export default function HomePage() {
   useEffect(() => {
@@ -44,10 +44,11 @@ export default function HomePage() {
         <OurServicesSection />
         <LeadershipSection />
         <FutureSection />
+        <OurExperienceSection />
         <ContactSection />
+
         {/* <AboutSection /> */}
         {/* 
-        <WhyChooseUs />
 
         <GalleryContextProvider>
           <GallerySection />
