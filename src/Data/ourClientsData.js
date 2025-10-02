56907819b3262ade0226c2e3fbba3f7a93848547
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-10-02 18:47:54 +0300

    Add Clients Data

---
+++ src/Data/ourClientsData.js
@@ -0,0 +1,153 @@
+import logo_01 from "/ClientsPage/Alhua.png";
+import logo_02 from "/ClientsPage/AlUla.jpg";
+import logo_03 from "/ClientsPage/Arbian Drilling.png";
+import logo_04 from "/ClientsPage/Cisco-logo.png";
+import logo_05 from "/ClientsPage/CommScope.webp";
+import logo_06 from "/ClientsPage/Consolidated_Contractors_Company_Logo.png";
+import logo_07 from "/ClientsPage/Dell_Logo.png";
+import logo_08 from "/ClientsPage/honeywell.png";
+import logo_09 from "/ClientsPage/Hyundai_Engineering_&_Construction_logo.png";
+import logo_10 from "/ClientsPage/KPIL.NS_BIG.png";
+import logo_11 from "/ClientsPage/Larsen__Toubro_Logo.png";
+import logo_12 from "/ClientsPage/logo-sabic.png";
+import logo_13 from "/ClientsPage/Neom-Logo.png";
+import logo_14 from "/ClientsPage/Nesma_Partners_Logo_RGB.png";
+import logo_15 from "/ClientsPage/NESR_BIG.png";
+import logo_16 from "/ClientsPage/Petrojet_logo.png";
+import logo_17 from "/ClientsPage/RedSea.jpg";
+import logo_18 from "/ClientsPage/Royal_Saudi_Air_Force_embelm.png";
+import logo_19 from "/ClientsPage/saipem-vector-logo.png";
+import logo_20 from "/ClientsPage/Saudi-Aramco-Logo.png";
+import logo_21 from "/ClientsPage/Saudi-Oger-Ltd-Logo-Vector.svg-.png";
+import logo_22 from "/ClientsPage/sisco_logo_1-200x122.png";
+import logo_23 from "/ClientsPage/SK_logo.svg.png";
+import logo_24 from "/ClientsPage/Yclogo.png";
+import logo_25 from "/ClientsPage/zamil.png";
+
+export const clients = [
+  {
+    id: `client-${logo_01}`,
+    imgSrc: logo_01,
+    alt: `client-${logo_01}`,
+  },
+  {
+    id: `client-${logo_02}`,
+    imgSrc: logo_02,
+    alt: `client-${logo_02}`,
+  },
+  {
+    id: `client-${logo_03}`,
+    imgSrc: logo_03,
+    alt: `client-${logo_03}`,
+  },
+  {
+    id: `client-${logo_04}`,
+    imgSrc: logo_04,
+    alt: `client-${logo_04}`,
+  },
+  {
+    id: `client-${logo_05}`,
+    imgSrc: logo_05,
+    alt: `client-${logo_05}`,
+  },
+  {
+    id: `client-${logo_06}`,
+    imgSrc: logo_06,
+    alt: `client-${logo_06}`,
+  },
+  {
+    id: `client-${logo_07}`,
+    imgSrc: logo_07,
+    alt: `client-${logo_07}`,
+  },
+  {
+    id: `client-${logo_08}`,
+    imgSrc: logo_08,
+    alt: `client-${logo_08}`,
+  },
+  {
+    id: `client-${logo_09}`,
+    imgSrc: logo_09,
+    alt: `client-${logo_09}`,
+  },
+  {
+    id: `client-${logo_10}`,
+    imgSrc: logo_10,
+    alt: `client-${logo_10}`,
+  },
+  {
+    id: `client-${logo_11}`,
+    imgSrc: logo_11,
+    alt: `client-${logo_11}`,
+  },
+  {
+    id: `client-${logo_12}`,
+    imgSrc: logo_12,
+    alt: `client-${logo_12}`,
+  },
+  {
+    id: `client-${logo_13}`,
+    imgSrc: logo_13,
+    alt: `client-${logo_13}`,
+  },
+  {
+    id: `client-${logo_14}`,
+    imgSrc: logo_14,
+    alt: `client-${logo_14}`,
+  },
+  {
+    id: `client-${logo_15}`,
+    imgSrc: logo_15,
+    alt: `client-${logo_15}`,
+  },
+  {
+    id: `client-${logo_16}`,
+    imgSrc: logo_16,
+    alt: `client-${logo_16}`,
+  },
+  {
+    id: `client-${logo_17}`,
+    imgSrc: logo_17,
+    alt: `client-${logo_17}`,
+  },
+  {
+    id: `client-${logo_18}`,
+    imgSrc: logo_18,
+    alt: `client-${logo_18}`,
+  },
+  {
+    id: `client-${logo_19}`,
+    imgSrc: logo_19,
+    alt: `client-${logo_19}`,
+  },
+  {
+    id: `client-${logo_20}`,
+    imgSrc: logo_20,
+    alt: `client-${logo_20}`,
+  },
+  {
+    id: `client-${logo_21}`,
+    imgSrc: logo_21,
+    alt: `client-${logo_21}`,
+  },
+  {
+    id: `client-${logo_22}`,
+    imgSrc: logo_22,
+    alt: `client-${logo_22}`,
+  },
+  {
+    id: `client-${logo_23}`,
+    imgSrc: logo_23,
+    alt: `client-${logo_23}`,
+  },
+  {
+    id: `client-${logo_24}`,
+    imgSrc: logo_24,
+    alt: `client-${logo_24}`,
+  },
+  {
+    id: `client-${logo_25}`,
+    imgSrc: logo_25,
+    alt: `client-${logo_25}`,
+  },
+];
