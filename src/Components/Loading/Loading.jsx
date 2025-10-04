87b06c163f2397a2c3aecd58e7f2fbc4b32af7eb
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-10-04 05:36:25 +0300

    Loading Component

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,63 +1,11 @@
-import React, { useRef } from "react";
-import { cn } from "../../lib/utils";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-
-gsap.registerPlugin(ScrollTrigger);
+import { SyncLoader } from "react-spinners";
 
 export default function Loading() {
-  const scrollRef = useRef();
-
-  // Implement Gsap.to() method
-  useGSAP(() => {
-    const boxes = gsap.utils.toArray(scrollRef.current.children);
-    boxes.forEach((box)=>{
-      gsap.to(box, {
-        x: 150,
-        rotation: 360,
-        borderRadius: '100%',
-        scale: 1.5,
-        scrollTrigger: {
-          trigger : box,
-          start: 'bottom bottom',
-          end: 'top 20%',
-          scrub: true
-        },
-        ease: 'power2.Out'
-      })
-    })
-  }, []);
-
   return (
     <>
-      <section className="h-screen bg-slate-700 text-white px-20 py-20">
-        <div
-          ref={scrollRef}
-          className={cn("container bg-red-900 h-full ", "flex items-center")}
-        >
-          <div
-            className="box w-52 h-52 bg-black rounded-lg"
-            id="blackBox"
-          ></div>
-          <div
-            className="box w-52 h-52 bg-white rounded-lg"
-            id="whiteBox"
-          ></div>
-        </div>
+      <section className="h-screen bg-white flex justify-center items-center text-secColor">
+        <SyncLoader color="currentColor" />
       </section>
     </>
   );
 }
-
-/* 
-useGSAP(() => {
-    gsap.to("#box", {
-      x: 1000,
-      repeat: -1,
-      yoyo: true,
-      rotation: 360,
-      duration: 2,
-    });
-  }, []);
-*/
