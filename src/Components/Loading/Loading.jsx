f4f608029af724e529ad81ce53b5cdc7415beddb
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-10-04 05:37:49 +0300

    Edit Loading

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import { SyncLoader } from "react-spinners";
 export default function Loading() {
   return (
     <>
-      <section className="h-screen bg-white flex justify-center items-center text-secColor">
+      <section className="h-screen bg-white flex justify-center items-center text-secColor relative z-[99999999]">
         <SyncLoader color="currentColor" />
       </section>
     </>
